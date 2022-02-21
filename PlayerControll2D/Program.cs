d308ce84ffcad7cd5d4402f00c528310889fc179
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-21 13:56:25 -0300

    135620022022

---
+++ PlayerControll2D/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlayerControll2D
+{
+    class PlayerController2D : MonoBehaviour
+    {
+        /*Use CharacterController2D*/
+        public CharacterController controller;
+        public float horizontalMove = 0f;
+        public float runSpeed = 40f;
+        bool jump = false;
+        bool crouch = false;
+
+        void Start()
+        {
+
+        }
+
+        void Updade()
+        {
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+            if (Input.GetButtonDown("Jump"))
+            {
+                
+                jump = true;
+            }
+
+            if (Input.GetButtonDown("Crouch"))
+            {
+                crouch = true;
+            }
+            else if (Input.GetButtonUp("Crouch"))
+            {
+                crouch = false;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            //Move Player
+            controller.Move(horizontalMove * Time.deltaTime, crouch, jump);
+            jump = false;
+        }
+    }
+}
